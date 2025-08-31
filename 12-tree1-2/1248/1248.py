2f02f1718f6d8655bec578181479865edcb39a38
ajjoona <ajjoona@gmail.com>
2025-08-31 17:07:50 +0900

    [1248] 공통 조상

--- 12-tree1-2/1248/1248.py
+++ 12-tree1-2/1248/1248.py
@@ -1,37 +1,35 @@
 # 1248. 공통조상
 
-import sys
-sys.stdin = open("input.txt")
+# import sys
+# sys.stdin = open("input.txt")
 
 
 def dfs_count(start):
-    """start부터 시작해서 DFS로 탐색한 노드의 개수 반환하는 함수"""
-    visited = [False] * (V + 1)
-    count = 0
+    """start부터 시작해서 DFS로 탐색한 서브트리의 노드의 개수를 반환하는 함수"""
+    visited = set()
 
-    visited[start] = True
+    visited.add(start)
     stack = [start]
     
     while stack:
         current_node = stack.pop()
-        count += 1
 
         for next_node in children[current_node]:
-            if not visited[next_node]:
-                visited[next_node] = True
+            if next_node not in visited:
+                visited.add(next_node)
                 stack.append(next_node)
     
-    return count
+    return len(visited)
 
 
 def find_ancestor(node):
-    """node의 조상 노드들을 set 형태로 반환하는 함수"""
-    ancestors = set()
+    """node의 조상 노드들을 list 형태로 반환하는 함수"""
+    ancestors = []
 
     # 부모 노드가 없을 때까지 반복
     while parents[node] != 0:
         node = parents[node]
-        ancestors.add(node)
+        ancestors.append(node)
     
     return ancestors
 
@@ -57,8 +55,13 @@ for tc in range(1, T+1):
     n1_ancs = find_ancestor(N1)
     n2_ancs = find_ancestor(N2)
     
-    # 두 정점의 조상 리스트 중에 공통 조상 번호 중 가장 큰 값
-    common_ancs = max(n1_ancs.intersection(n2_ancs))
+    # 두 정점의 조상 리스트 중에 공통 조상 번호 중 가장 가까운 값
+    # 노드번호는 크기순이 아니므로, 인덱스가 작은 값이 가장 가까운 값이다.
+    # common_ancs = max(n1_ancs.intersection(n2_ancs))
+    for n1_anc in n1_ancs:
+        if n1_anc in n2_ancs:
+            common_ancs = n1_anc
+            break
 
     # common_ancs의 서브 트리의 길이를 구한다.
     subtree_size = dfs_count(common_ancs)
